8b85cf6a51f8dbdb6c8fa545dd93491d908b22cc
deevaah <deevnyy@gmail.com>
2022-11-11 00:11:03 +0300

    first commit

---
+++ 4.py
@@ -0,0 +1,19 @@
+def fib():
+    fibon = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+    i = iter(fibon)
+    for x in i:
+        print(x, end = " ")
+
+
+fib()
+
+print()
+
+ibon():
+    fibon = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+    i = iter(fibon)
+    while i != 10:
+        print(next(i), end = " ")
+
+
+fibon()
